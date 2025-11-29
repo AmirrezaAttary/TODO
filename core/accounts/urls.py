5e8cbe9ff04439aae17cb1277b97945a75f1f86c
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-11-29 11:36:36 +0330

    feat: Integrate Celery for asynchronous task handling and add email sending functionality

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import CustomLoginView, RegisterPage
+from .views import CustomLoginView, RegisterPage, send_email
 
 from django.contrib.auth.views import LogoutView
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
     path("register/", RegisterPage.as_view(), name="register"),
+    path('send_email',send_email,name='send_email'),
     path('api/v1/',include('accounts.api.v1.urls'))
 ]
